93253e800ce63b734d2f7e881e66bdf5bba6e975
VyomSehgal16 <70372266+VyomSehgal16@users.noreply.github.com>
2020-08-29 15:06:37 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,68 @@
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage;
+var CloudsGroup,CloudImage
+
+function preload(){
+  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
+  trex_collided = loadImage("trex_collided.png");
+  CloudImage = loadImage("cloud.png");
+  groundImage = loadImage("ground2.png");
+}
+
+function setup() {
+  createCanvas(600, 200);
+  
+  trex = createSprite(50,180,20,50);
+  trex.addAnimation("running", trex_running);
+  trex.scale = 0.5;
+  
+  ground = createSprite(200,180,400,20);
+  ground.addImage("ground",groundImage);
+  ground.x = ground.width /2;
+  ground.velocityX = -2;
+  
+  invisibleGround = createSprite(200,190,400,10);
+  invisibleGround.visible = false;
+}
+
+function draw() {
+  background(170);
+  
+  if(keyDown("space")) {
+    trex.velocityY = -10;
+  }
+  
+  trex.velocityY = trex.velocityY + 0.8
+  
+  if (ground.x < 0){
+    ground.x = ground.width/2;
+  }
+  
+  trex.collide(invisibleGround);
+  
+  spawnClouds();
+  
+  drawSprites();
+}
+
+function spawnClouds() {
+  //write code here to spawn the clouds
+  if (frameCount % 60 === 0) {
+    var cloud = createSprite(600,110,40,10);
+    cloud.y = Math.round(random(20,110));
+    cloud.addImage("CloudImage");
+    cloud.scale = 0.5;
+    cloud.velocityX = -3;
+    
+     //assign lifetime to the variable
+    cloud.lifetime = 200;
+    
+    //adjust the depth
+    cloud.depth = trex.depth;
+    trex.depth = trex.depth + 1;
+    
+    //add each cloud to the group
+    CloudsGroup.add(cloud);
+  }
+  
+}
